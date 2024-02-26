20c85cf64727b0706dc588c5b434a148419d4fc8
PermeableMaple1 <123516926+PermeableMaple1@users.noreply.github.com>
2024-02-25 18:01:01 -0800

    Add files via upload

---
+++ data/map/lost_halls_tileset.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="lost_halls" tilewidth="32" tileheight="32" tilecount="12" columns="3">
+ <image source="lost_halls_tileset.png" width="96" height="128"/>
+ <tile id="11">
+  <objectgroup draworder="index" id="2">
+   <object id="4" x="0" y="0" width="32" height="32"/>
+   <object id="5" x="15.375" y="14.625">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
